17966d31b007321e5d8af87c146681acb91b4c9c
Mohamed <mohamedabdelfatah2027@gmail.com>
2020-12-19 16:53:08 +0200

    pre-test finish

---
+++ Project 1/a/wcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdlib.h>
+#define size (512)
+int main(int argc , char *argv[]){
+    if(argc == 1){
+        return 0;
+    }
+    for(int i = 1 ; i < argc ;i++){
+
+        FILE *fil = fopen(argv[i] , "r");
+        if(fil == NULL){
+            printf("wcat: cannot open file");
+            exit(1);
+        }
+
+
+        char buffer[size];
+        while(fgets(buffer , size , fil) != NULL){
+            printf("%s" , buffer);
+        }
+        printf("\n");
+
+        fclose(fil);
+    }
+    return 0;
+
+}
+
